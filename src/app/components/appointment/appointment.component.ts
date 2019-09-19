6205b02104dc41cee113a56faa77c410d8a94209
milanszakacs <milan.szakacs@gmail.com>
2019-09-19 16:42:59 +0200

    Events can be 30minute

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { Event } from 'src/app/models/Event';
 
-
 declare var eventStarts: string[];
 declare var eventEnds: string[];
-
-
 
 @Component({
   selector: 'app-appointment',
@@ -24,50 +21,58 @@ export class AppointmentComponent implements OnInit {
 
   ngOnInit() {
     this.initEventsList();
-    // console.log(events);
-    this.initFreeEventsList('2019-09-28', 1);
+    this.initFreeEventsList('2019-09-28', 1, 30);
     console.log(this.freeEvents);
   }
-  
 
-  //Filling events list with the events already exists in the calendar
+  // Filling events list with the events already exists in the calendar
   initEventsList() {
     for (let i = 0; i < eventStarts.length; i++) {
       this.events.push(new Event(moment(eventStarts[i]['dateTime']), moment(eventEnds[i]['dateTime'])));
-      // console.log(events[i]);
     }
   }
 
   // Find the free slots and fill the eventList with it
   // @param1: The date when freeslots needed
   // @param2: The length of the slot in Hour
-  initFreeEventsList(expectedDate: string, slotLength: number) {
+  initFreeEventsList(expectedDate: string, slotLengthHour: number, slotLengthMinute: number) {
     const openingTime = 8;
     const closingTime = 20;
-    let tempMinute = 0;
+    let startMinute = 0;
+    let endMinute = slotLengthMinute;
+    let tempHour = 0;
     for (let i = openingTime; i < closingTime;) {
       let isBefore = true;
       let tempEvent = new Event(
-        moment(expectedDate).hour(i).minute(tempMinute), moment(expectedDate).hour(i + slotLength).minute(tempMinute));
-      this.events.forEach(element => {
-        if (moment(element.startTime).date() === 28) {
-          if (moment(element.startTime).isSame(moment(tempEvent.startTime))) {
-            isBefore = false;
-            i++;
-      
-          }
+        moment(expectedDate).hour(i).minute(startMinute),
+        moment(expectedDate).hour(i + slotLengthHour + tempHour).minute(endMinute));
+        startMinute = endMinute;
+        endMinute += slotLengthMinute;
+        console.log('Endminute: ' + endMinute);
+      if (endMinute == 60) {
+        endMinute = 0;
+        tempHour = 1;
+      } else {
+        if (slotLengthMinute !== 0){
+          tempHour = 0;
+          i += 1;
         }
-      });
-      if (isBefore) {
-        this.freeEvents.push(tempEvent);
-        i += slotLength;
       }
-    
-    }  
+      i += slotLengthHour;
+      this.freeEvents.push(tempEvent);
 
+      // this.events.forEach(element => {
+      //   if (moment(element.startTime).date() === 28) {
+      //     if (moment(element.startTime).isSame(moment(tempEvent.startTime))) {
+      //       isBefore = false;
+      //       i++;
+      //     }
+      //   }
+      // });
+      // if (isBefore) {
+      //   this.freeEvents.push(tempEvent);
+      //   i += slotLengthHour;
+      // }
+    }
   }
-
-
-
-
 }
